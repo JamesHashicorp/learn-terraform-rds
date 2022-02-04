5a8206fff2c27454509a0834e0cebe960eea2171
JamesHashicorp <jallen@hashicorp.com>
2022-02-04 09:22:57 +0000

    infrastructure spin up

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_db_subnet_group" "rds_subnet" {
   }
 }
 
-## comment here
+## comment here init
 resource "aws_security_group" "rds" {
   name   = var.project_name
   vpc_id = module.vpc.vpc_id
