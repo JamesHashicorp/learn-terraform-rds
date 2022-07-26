fb85e07a5ccf2b6863b43b92a8f2cfa4bfbe82e4
James Allen <87305562+JamesHashicorp@users.noreply.github.com>
2022-07-26 09:14:20 +0100

    new variable implemented into main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_db_subnet_group" "rds_subnet" {
   }
 }
 
-## comment  change made 
+## comment make change made 
 resource "aws_security_group" "rds" {
   name   = var.project_name
   vpc_id = module.vpc.vpc_id
